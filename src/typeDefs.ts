20fa1c98078ee25d91d24aaf8aaf203800f1eedd
Hyeungshik Jung <hyungshike@gmail.com>
2020-08-22 15:16:28 +0900

    feat: remove comments in typeDefs

--- src/typeDefs.ts
+++ src/typeDefs.ts
@@ -1,12 +1,6 @@
 const { gql } = require("apollo-server-koa");
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
 export const typeDefs = gql`
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Book" type defines the queryable fields for every book in our data source.
   type Post {
     id: Int!
     body: String
@@ -20,9 +14,6 @@ export const typeDefs = gql`
     lastName: String!
   }
 
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
     posts(pageSize: Int, skip: Int): [Post]
     post(id: ID!): Post
